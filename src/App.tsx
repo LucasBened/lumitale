358b2e42955ef8c7a72203d1ad25dffa22f45fc9
lucasbened <lussivbsilva1@gmail.com>
2025-03-25 20:51:55 -0300

    Criando repositório

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="flex justify-center text-4xl text-violet-500">
+        hello world
+      </h1>
+      <div className="flex justify-center rounded-md bg-red-500 p-2 text-white"></div>
+    </div>
+  );
+}
